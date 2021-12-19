aac268ab06b1325af850e81948935e7c8690fc4c
Duelawig <59671758+AA206yt@users.noreply.github.com>
2021-12-19 15:12:13 -0800

    Add files via upload

---
+++ clean all files.bat
@@ -0,0 +1,81 @@
+@echo off
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+del %appdata%\willishot\willishot1.png
+del %appdata%\willishot\windows.jpg
+del %appdata%\willishot\cmdmenusel.exe
+del %appdata%\willishot\2choise.bat
+del %appdata%\willishot\Messagebox.vbs
+del "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\batstart.bat"
+del %appdata%\willishot\Willisvirus2.bat
+del %appdata%\willishot\wallpaper.png
+del %appdata%\willishot\windows.jpg
+del %appdata%\willishot\2choise.bat
+del %temp%\bruh.mp3
+del %temp%\fnaf.mp3
+del %temp%\vine.mp3
+del %temp%\thanks.wav
+del %temp%\creepy.mp3
+del %temp%\wouldyousay.wav
+del %temp%\creepy2.mp3
+del %temp%\again.wav
+del %temp%\goodjob.wav
+del %temp%\tryagain.wav
+del %temp%\areyousure.mp3
+del %temp%\bruh.vbs
+del %temp%\fnaf.vbs
+del %temp%\vine.vbs
+del %temp%\thanks.vbs
+del %temp%\creepy.vbs
+del %temp%\wouldyousay.vbs
+del %temp%\creepy2.vbs
+del %temp%\again.vbs
+del %temp%\goodjob.vbs
+del %temp%\tryagain.vbs
+del %temp%\areyousure.vbs
+cd C:\Users\%username%\Desktop
+del "nice ip0.txt" >nul
+del "nice ip1.txt" >nul
+del "nice ip2.txt" >nul
+del "nice ip3.txt" >nul
+del "nice ip4.txt" >nul
+del "nice ip5.txt" >nul
+del "nice ip6.txt" >nul
+del "nice ip7.txt" >nul
+del "nice ip8.txt" >nul
+del "nice ip9.txt" >nul
+del "nice ip10.txt" >nul
+del "nice ip11.txt" >nul
+del "nice ip12.txt" >nul
+del "nice ip13.txt" >nul
+del "nice ip14.txt" >nul
+del "nice ip15.txt" >nul
+del "nice ip16.txt" >nul
+del "nice ip17.txt" >nul
+del "nice ip18.txt" >nul
+del "nice ip19.txt" >nul
+del "nice ip20.txt" >nul
+del "nice ip21.txt" >nul
+del "nice ip22.txt" >nul
+del "nice ip23.txt" >nul
+del "nice ip24.txt" >nul
+del "nice ip25.txt" >nul
+del "nice ip26.txt" >nul
+del "nice ip27.txt" >nul
+del "nice ip28.txt" >nul
+del "nice ip29.txt" >nul
+del "nice ip30.txt" >nul
+del "nice ip31.txt" >nul
+del "nice ip32.txt" >nul
+del "nice ip33.txt" >nul
+del "nice ip34.txt" >nul
+del "nice ip35.txt" >nul
+del "nice ip36.txt" >nul
+del "nice ip37.txt" >nul
+del "nice ip38.txt" >nul
+del "nice ip39.txt" >nul
+del "nice ip40.txt" >nul
+cls
+echo Successfully cleaned all virus files.
+echo Have a great day.
+timeout 2 >nul
+exit
